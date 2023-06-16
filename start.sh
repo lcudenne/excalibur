b6c15d328042dfa50f29d3964a8d7695450bcf45
Loïc Cudennec <loic.cudennec@gmail.com>
2023-06-16 11:23:41 +0200

    do not check requirements at startup for offline/slow devices

--- start.sh
+++ start.sh
@@ -3,10 +3,11 @@
 if [ ! -d "venv" ]
 then
     python3 -m venv venv &> /dev/null
+    source venv/bin/activate &> /dev/null
+    pip install -r excalibur/requirements.txt &> /dev/null
+else
+    source venv/bin/activate &> /dev/null
 fi
-
-source venv/bin/activate &> /dev/null
-pip install -r excalibur/requirements.txt &> /dev/null
 
 PYTHONPATH+=. python3 excalibur/app.py $@ &> excalibur.log
 
